67dcd657bcbbeaa9555a4ea8c0231b758044704e
vijay mali <guddavijay@gmail.com>
2017-04-27 15:06:58 +0530

    tool news layout done

--- lib/Tool/SamajNews.php
+++ lib/Tool/SamajNews.php
@@ -6,6 +6,8 @@ namespace xavoc\allsamaj;
 class Tool_SamajNews extends \xepan\cms\View_Tool {
 
 	public $options=[
+					'no_of_record'=>10,
+					'no_of_column'=>' ',
 					'detail_url_page'=>null
 
 	];
@@ -18,12 +20,27 @@ class Tool_SamajNews extends \xepan\cms\View_Tool {
 		if($samaj_id){
 			$n->addCondition('samaj_id',$samaj_id);		
 		}
-		$lister = $this->add('CompleteLister',null,null,['view/samajnews1']);
+		$lister = $this->add('CompleteLister',null,null,['view/samajnews']);
 
 		$lister->addHook('formatRow',function($l){
+			if($this->options['no_of_column']){
+				$l->current_row_html['col'] = $this->options['no_of_column'];
+			}else{
+				$l->current_row_html['col'] = "";
+			}
+
 			if($this->options['detail_url_page']){
-				$l->current_row_html['news_details_page_url'] = $this->options['detail_url_page'];
-			}			
+				$l->current_row_html['detail_url'] = $this->options['detail_url_page'];
+			}
+
+			
+
+			if($this->options['detail_url_page']){
+				$l->current_row_html['detail_url'] = $this->options['detail_url_page'];
+			}	
+
+			$l->current_row_html['content'] = $l->model['content'];		
+			$l->current_row_html['date'] = date('F d, Y',strtotime($l->model['date']));
 		});
 		
 		$lister->setModel($n);
