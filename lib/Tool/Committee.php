ebfd773289dd4e169d6a5409842730e1f7ce106e
vijay mali <guddavijay@gmail.com>
2017-04-29 18:58:41 +0530

    tool options layout modified

--- lib/Tool/Committee.php
+++ lib/Tool/Committee.php
@@ -6,7 +6,12 @@ namespace xavoc\allsamaj;
 * 
 */
 class Tool_Committee extends \xepan\cms\View_Tool{
-	public $options=[];
+	public $options=[
+					'show_image'=>'',
+					'detail_url'=>'',
+					'no_of_record'=>10,
+
+	];	
 	function init(){
 		parent::init();
 		$samaj_id = $this->app->stickyGET('samaj_id');
@@ -15,7 +20,20 @@ class Tool_Committee extends \xepan\cms\View_Tool{
 			$m->addCondition('samaj_id',$samaj_id);
 
 		$l = $this->add('CompleteLister',null,null,['view/committee']);
+
+		$l->addHook('formatRow',function($l){
+			if($this->options['detail_url']){
+				$l->current_row_html['detail_url'] = $this->app->url($this->options['detail_url_page'],['committee_id'=>$l->model->id]);
+			}
+		});
+		
 		$l->setModel($m);			
+
+		if($this->options['no_of_record']){
+			$paginator = $l->add('Paginator',null,'paginator');
+			$paginator->setRowsPerPage($this->options['no_of_record']);
+
+		}
 	}
 
 	// function defaultTemplate(){
