0a5148aaab390915555d6d81f82a1410f00e38db
vijay mali <guddavijay@gmail.com>
2017-05-16 19:12:43 +0530

    tool search options & layout modified

--- lib/Tool/Search.php
+++ lib/Tool/Search.php
@@ -7,13 +7,13 @@ namespace xavoc\allsamaj;
 */
 class Tool_Search extends \xepan\cms\View_Tool{
 	public $options = [
-					'form_layout'=>'view/serchform',
+					'form_layout'=>'view/searchform',
 					'xavoc_allsamaj_search_result_page'=>"index",
-					// 'search-field-label'=>"Type Your Search String",
+					'search-field-label'=>"Type Your Search String",
 					'search-form-btn'=>true,
-					'search-form-btn-label'=>'Search',
-					'search-button-position'=>"after",
-					'search-form-btn-icon'=>"glyphicon glyphicon-search"
+					// 'search-form-btn-label'=>'Search',
+					// 'search-button-position'=>"after",
+					// 'search-form-btn-icon'=>"glyphicon glyphicon-search"
 					];
 	function init(){
 		parent::init();
@@ -30,8 +30,8 @@ class Tool_Search extends \xepan\cms\View_Tool{
 		if($this->options['search-field-label'] != "")
 			$label = $this->options['search-field-label'];
 
-		$f = $this->add('Form');;
-		// $f->setLayout($this->options['form_layout']);
+		$f = $this->add('Form',null,null,['form/empty']);;
+		$f->setLayout($this->options['form_layout']);
 
 		$search_samaj = $this->add('xavoc\allsamaj\Model_Samaj',['title_field'=>'search_string']);
 		// $search_field = $f->addField('autocomplete\Basic','search');
@@ -42,6 +42,7 @@ class Tool_Search extends \xepan\cms\View_Tool{
 		// $form = $this->add('Form');
 	   	
 	   	if($this->options['search-form-btn']){
+	   		$f->addSubmit(' ')->addClass(' btn btn-default glyphicon  glyphicon-search');
 	   		if($this->options['search-button-position'] === "before")
 	  	 		$submit_button = $search_field->beforeField()->add('Button');
 	  	 	else
